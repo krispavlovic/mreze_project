5a963ebbb1f6c1ad9f978d35d6d22d786c3ae22e
krispavlovic <kristina33690@gmail.com>
2025-07-09 22:14:09 +0200

    init

---
+++ Klijent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MainClient
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MainServer
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint hostingEndpoint = new IPEndPoint(IPAddress.Any, 11552);
+
+            serverSocket.Bind(hostingEndpoint);
+            serverSocket.Blocking = false;
+
+            Console.WriteLine($"Server je pokrenut i ceka poruku na :{hostingEndpoint}");
+        }
+    }
+}
