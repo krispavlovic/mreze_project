7a6116974afd8cf80e3cb88213168499f0ebf80e
krispavlovic <kristina33690@gmail.com>
2025-07-10 04:33:23 +0200

    init konobar

--- Klijent/obj/Debug/net8.0/Klijent.AssemblyInfo.cs
+++ Klijent/obj/Debug/net8.0/Klijent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Klijent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a963ebbb1f6c1ad9f978d35d6d22d786c3ae22e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Klijent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Klijent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
